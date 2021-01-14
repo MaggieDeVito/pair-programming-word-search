8a40ddd547591526c123a89b5ec7e99de4218b33
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-14 01:17:52 +0000

    made the function work

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
 const wordSearch = (letters, word) => { 
+    let array = [];
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    for (const rows of letters) {
+        array.push([])
+    }
+
+    for(let i = 0; i < letters.length; i++) { 
+        for(let j = 0; j < letters[i].length; j++) {
+            array[j].push(letters[i][j]);
+        }
+    }
+
+    const verticalJoin = array.map(ls => ls.join(''))
+    for(el of verticalJoin) {
+        if (el.includes(word)) return true
+
+    } return false;
 }
 
 module.exports = wordSearch
